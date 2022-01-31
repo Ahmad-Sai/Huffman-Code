994a63d8b2513296b060b6f81ee3e6f66253b5dd
Ahmad-Sai <85080576+Ahmad-Sai@users.noreply.github.com>
2022-01-31 13:14:43 -0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <fstream>
+#include <math.h>
+#include <bitset>
+#include "priorityqueue.hpp"
+#include "encoder.cpp"
+#include "decoder.cpp"
+
+using namespace std;
+
+int main(){
+    
+    encode();
+    decode();
+    return 1;
+}
